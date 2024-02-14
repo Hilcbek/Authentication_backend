9ac0dbf0efff16469eed35a0f9444103ad92e99e
Hilcbek <balemayehu07@gmail.com>
2024-02-14 23:00:41 +0300

    adadasdasdaasdassdada

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://auth-mern-mu.vercel.app/","http://localhost:5173"],
     credentials: true,
   })
 );
